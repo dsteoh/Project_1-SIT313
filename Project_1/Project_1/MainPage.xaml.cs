841e3d0b7f5fcda8f484a2c1774cb34e9ae40b21
dsteoh <desiangteoh@gmail.com>
2017-09-15 16:25:09 +1000

    changed c# to xmal coded Tabbed Page

--- Project_1/Project_1/MainPage.xaml.cs
+++ Project_1/Project_1/MainPage.xaml.cs
@@ -12,34 +12,7 @@ namespace Project_1
         public MainPage()
         {
             InitializeComponent();
-
-            this.Children.Add(new TopicPage());
-            this.Children.Add(new SearchPage());
-            this.Children.Add(new ProfilePage());
-            this.Children.Add(new SettingsPage());
-
         }
-
-        ////Navigation buttons
-        //async void btnTopic_Clicked(object sender, EventArgs e)
-        //{
-        //    await Navigation.PushAsync(new TopicPage());    
-        //}
-
-        //async void btnSearch_Click(object sender, EventArgs e)
-        //{
-        //    await Navigation.PushAsync(new SearchPage());
-        //}
-
-        //async void btnProfile_Click(object sender, EventArgs e)
-        //{
-        //    await Navigation.PushAsync(new ProfilePage());
-        //}
-
-        //async void btnSettings_Click(object sender, EventArgs e)
-        //{
-        //    await Navigation.PushAsync(new SettingsPage());
-        //}
     }
 }
 
